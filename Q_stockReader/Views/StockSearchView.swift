7c3f575469186fa2de4b6b51ab8b5a3c7ba7757a
Philippe Blanchette <philippe.blanchette@nventive.com>
2025-04-29 15:41:20 -0400

    updated the project structur

--- Q_stockReader/Views/StockSearchView.swift
+++ Q_stockReader/Views/StockSearchView.swift
@@ -16,55 +16,6 @@ struct StockView: View {
     }
 }
 
-protocol SearchBarManager: ObservableObject {
-    @MainActor func onInputTextChange(inputText text: String)
-    @MainActor func clearSearch()
-    var inputText: String { get set }
-}
-
-struct SearchBarView<VM>: View where VM: SearchBarManager {
-    
-    @ObservedObject var viewModel: VM
-    @FocusState private var focus: Bool
-    
-    var body: some View {
-        HStack {
-            HStack {
-                Image(systemName: "magnifyingglass")
-                    .padding(.leading, 12)
-                
-                TextField(
-                    text: $viewModel.inputText,
-                    prompt: Text("Search for ticker or company name"),
-                    label: {
-                        Text("Search")
-                    }
-                )
-                .focused($focus)
-                .onChange(of: viewModel.inputText, perform: { newValue in
-                    if focus && !newValue.isEmpty {
-                        viewModel.onInputTextChange(inputText: newValue)
-                    }
-                })
-                .padding(.vertical, 12)
-                
-                if !viewModel.inputText.isEmpty {
-                    Image(systemName: "xmark.circle")
-                        .padding(.trailing, 12)
-                        .onTapGesture {
-                            self.viewModel.clearSearch()
-                        }
-                }
-            }
-            .background(
-                RoundedRectangle(cornerRadius: 12)
-                    .stroke(Color.gray, lineWidth: 1)
-            )
-        }
-
-    }
-}
-
 struct StockSearchView: View {
     @StateObject var viewModel: StockViewModel
     
