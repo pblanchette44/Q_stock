0cd2ae0a3f3faed3d8d653f6f0dd8bfa2947623c
Philippe Blanchette <philippe.blanchette@nventive.com>
2025-04-29 07:50:12 -0400

    first commit

---
+++ Q_stockReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Q_stockReader
+//
+//  Created by Philippe Blanchette on 2025-04-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Q_stockReader/Q_stockReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Q_stockReaderApp.swift
+//  Q_stockReader
+//
+//  Created by Philippe Blanchette on 2025-04-29.
+//
+
+import SwiftUI
+
+@main
+struct Q_stockReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
